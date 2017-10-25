d5de44b99d657b16fb8abbdd5693e3947b27c469
Dalencourt Alex <dalencourt@b10p15.fil.univ-lille1.fr>
2017-10-25 18:25:53 +0200

    clean

--- src/main/resources/db/sql/create-db-message.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE message IF EXISTS;
-
-CREATE TABLE message (
-  id         INTEGER PRIMARY KEY,
-  sender VARCHAR(50),
-  content  VARCHAR(50)
-);
--- src/main/resources/db/sql/insert-data-message.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO message VALUES (1, 'mkyong', 'contenu du message');
-INSERT INTO message VALUES (2, 'alex', 'messagerez');
-INSERT INTO message VALUES (3, 'joel', 'rtetrezre');
