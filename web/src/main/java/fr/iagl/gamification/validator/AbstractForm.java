d93da5ad4ce1155849b9b2c14e72c61e34513d0f
TheMotain <alex.dalencourt@yahoo.fr>
2017-11-06 19:53:29 +0100

    StudentForm

---
+++ web/src/main/java/fr/iagl/gamification/validator/AbstractForm.java
@@ -0,0 +1,10 @@
+package fr.iagl.gamification.validator;
+
+/**
+ * Classe abstraite sans corps permettant de donner un type commun à tous les validateurs
+ * @author ALEX
+ *
+ */
+public abstract class AbstractForm {
+
+}
