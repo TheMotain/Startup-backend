edeec1236bebf1dd2846ec2315088c8c18ae50fe
Dalencourt Alex <dalencourt@b10p15.fil.univ-lille1.fr>
2017-11-08 16:03:10 +0100

    conf bundle

--- web/src/main/java/fr/iagl/gamification/constants/CodeError.java
+++ web/src/main/java/fr/iagl/gamification/constants/CodeError.java
@@ -2,7 +2,7 @@ package fr.iagl.gamification.constants;
 
 public class CodeError {
 
-	public static final String ERROR_EXISTS_CLASS = "La classe existe déjà.";
+	public static final String ERROR_EXISTS_CLASS = "${error.exist.class}";
 	public static final String ERROR_SIZE_2_30_CLASSNAME = "Le nom de la classe doit avoir entre {min} et {max} caractères.";
 	public static final String ERROR_PATTERN_CLASSNAME = "Le nom de la classe ne doit contenir que des caractères alphanumériques et - et _.";
 	public static final String ERROR_NULL_CLASSNAME = "Entrez un nom de classe";
