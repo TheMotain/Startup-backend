dd5d705bfb27a533baa8e4770d4bbaeac87617c3
Meyer Helene <helene.meyer@etudiant.univ-lille1.fr>
2017-11-15 17:25:00 +0100

    modif repository

--- data/src/main/java/fr/iagl/gamification/repository/StudentRepository.java
+++ data/src/main/java/fr/iagl/gamification/repository/StudentRepository.java
@@ -1,9 +1,24 @@
 package fr.iagl.gamification.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.iagl.gamification.entity.StudentEntity;
 
+/**
+ * Repository pour les appels dans la classe pupil
+ * 
+ * @author ALEX
+ *
+ */
 public interface StudentRepository extends CrudRepository<StudentEntity, Long> {
+	
+	/**
+	 * retourne tous les élèves qui n'ont pas de classe
+	 * 
+	 * @return tous les élèves qui n'ont pas de classe
+	 */
+	List<StudentEntity> findByClassroomIsNull();
 	
 }
