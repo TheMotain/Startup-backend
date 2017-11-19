eb6ce1be2d5bf7be98405f9077a15adf8749c4ac
HeleneMyr <mey.helene@gmail.com>
2017-11-20 00:40:15 +0100

    remove unused import

--- data/src/main/java/fr/iagl/gamification/repository/StudentRepository.java
+++ data/src/main/java/fr/iagl/gamification/repository/StudentRepository.java
@@ -1,7 +1,5 @@
 package fr.iagl.gamification.repository;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 import fr.iagl.gamification.entity.StudentEntity;
--- web/src/main/java/fr/iagl/gamification/controller/StudentController.java
+++ web/src/main/java/fr/iagl/gamification/controller/StudentController.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.validation.Valid;
@@ -27,7 +26,6 @@ import fr.iagl.gamification.exceptions.ClassroomAlreadyExistedException;
 import fr.iagl.gamification.exceptions.ClassroomNotFoundException;
 import fr.iagl.gamification.exceptions.StudentNotFoundException;
 import fr.iagl.gamification.model.StudentModel;
-import fr.iagl.gamification.object.ClassObject;
 import fr.iagl.gamification.object.StudentObject;
 import fr.iagl.gamification.services.StudentService;
 import fr.iagl.gamification.utils.RequestTools;
