79d36cb37c48b98272255e96207e9fbbe8bfd8db
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2017-12-04 19:11:45 +0100

    Tests du converter + conf sonar

--- body/src/main/java/fr/iagl/gamification/converter/CustomStudentPointsFieldsConverter.java
+++ body/src/main/java/fr/iagl/gamification/converter/CustomStudentPointsFieldsConverter.java
@@ -4,41 +4,59 @@ import org.dozer.CustomConverter;
 import org.dozer.MappingException;
 
 import fr.iagl.gamification.entity.PointEntity;
+import fr.iagl.gamification.entity.StudentEntity;
 import fr.iagl.gamification.model.PointModel;
+import fr.iagl.gamification.model.StudentModel;
 
+/**
+ * Converter customisé pour le mapping de l'attribut
+ * {@link StudentModel#getPoints()} {@link StudentEntity#getPoints()}
+ * 
+ * @author dalencourt
+ *
+ */
 public class CustomStudentPointsFieldsConverter implements CustomConverter {
 
+	/**
+	 * Surcharge de la méthode {@link CustomConverter#convert(Object, Object, Class, Class)}<br/>
+	 * Ne mappe pas l'attribut Student contenu dans l'attribut point<br/>
+	 * Si l'attribut point est null :
+	 * <ul>
+	 * <li>Model -> Entity : le PointEntity n'est pas mappé</li>
+	 * <li>Entity -> Model : le PointModel est créé avec la valeur par défaut 0 dans chaque attribut</li>
+	 * </ul>
+	 */
 	@Override
 	public Object convert(Object destination, Object source, Class<?> classDest, Class<?> classSource) {
-		if(source == null && PointModel.class.equals(classSource)) {
+		if (source == null && !PointEntity.class.equals(classSource)) {
 			return null;
 		}
-		if(PointModel.class.equals(classSource)) {
+		if (PointModel.class.equals(classSource)) {
 			PointEntity dest = null;
-			if(destination == null) {
+			if (destination == null) {
 				dest = new PointEntity();
 			} else {
 				dest = (PointEntity) destination;
 			}
-			dest.setBonus(((PointModel)source).getBonus());
-			dest.setMalus(((PointModel)source).getMalus());
+			dest.setBonus(((PointModel) source).getBonus());
+			dest.setMalus(((PointModel) source).getMalus());
 			return dest;
-		}else if(PointEntity.class.equals(classSource)) {
+		} else if (PointEntity.class.equals(classSource)) {
 			PointModel dest = null;
-			if(destination == null) {
+			if (destination == null) {
 				dest = new PointModel();
 			} else {
 				dest = (PointModel) destination;
 			}
-			if(source != null) {
-				dest.setBonus(((PointEntity)source).getBonus());
-				dest.setMalus(((PointEntity)source).getMalus());
+			if (source != null) {
+				dest.setBonus(((PointEntity) source).getBonus());
+				dest.setMalus(((PointEntity) source).getMalus());
 			} else {
 				dest.setBonus(0);
 				dest.setMalus(0);
 			}
 			return dest;
-		}else {
+		} else {
 			throw new MappingException("Converter CustomStudentPointsFieldsConverter "
 					+ "used incorrectly. Arguments passed in were:" + destination + " and " + source);
 		}
---
+++ body/src/test/java/fr/iagl/gamification/converter/CustomStudentPointsFieldsConverterTest.java
@@ -0,0 +1,67 @@
+package fr.iagl.gamification.converter;
+
+import org.dozer.MappingException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import fr.iagl.gamification.entity.PointEntity;
+import fr.iagl.gamification.model.PointModel;
+
+public class CustomStudentPointsFieldsConverterTest {
+	@Test
+	public void testInputIsNullAndNullSourceTypeThenReturnNullPointEntity() {
+		Assert.assertNull(new CustomStudentPointsFieldsConverter().convert(null,null,null,null));
+	}
+	
+	@Test
+	public void testInputIsNotAndSourceTypeIsNotPointEntityThenReturnNullPointEntity() {
+		Assert.assertNull(new CustomStudentPointsFieldsConverter().convert(null,null,null,PointModel.class));
+	}
+	
+	@Test
+	public void testInputIsNullAndSourceTypeIsPointEntityThenReturnDefaultPointEntity() {
+		PointModel entity = (PointModel) new CustomStudentPointsFieldsConverter().convert(null,null,null,PointEntity.class);
+		Assert.assertNotNull(entity);
+		Assert.assertEquals(0, entity.getBonus());
+		Assert.assertEquals(0, entity.getMalus());
+		Assert.assertNull(entity.getStudent());
+	}
+	
+	@Test
+	public void testSourceisNotNullAndDestinationIsNotNullAndTypeSourceIsPointModelThenDontCreateNewDestination() {
+		PointEntity destination = new PointEntity();
+		Assert.assertEquals(destination, new CustomStudentPointsFieldsConverter().convert(destination, new PointModel(), null, PointModel.class));
+	}
+	
+	@Test
+	public void testSourceisNotNullAndDestinationIsNotNullAndTypeSourceIsPointEntityThenDontCreateNewDestination() {
+		PointModel destination = new PointModel();
+		Assert.assertEquals(destination, new CustomStudentPointsFieldsConverter().convert(destination, new PointEntity(), null, PointEntity.class));
+	}
+	
+	@Test(expected=MappingException.class)
+	public void testSourceisNotNullAndTypeSourceIsPointEntityAndPointModelThenDontCreateNewDestination() {
+		PointModel destination = new PointModel();
+		Assert.assertEquals(destination, new CustomStudentPointsFieldsConverter().convert(destination, new PointModel(), null, null));
+	}
+	
+	@Test
+	public void testMappingModelToEntityMapAttributes() {
+		PointModel model = new PointModel();
+		model.setBonus(1);
+		model.setMalus(2);
+		PointEntity entity = (PointEntity) new CustomStudentPointsFieldsConverter().convert(null, model, null, model.getClass());
+		Assert.assertEquals(model.getBonus(), entity.getBonus());
+		Assert.assertEquals(model.getMalus(), entity.getMalus());
+	}
+	
+	@Test
+	public void testMappingEntityToModelMapAttributes() {
+		PointEntity entity = new PointEntity();
+		entity.setBonus(1);
+		entity.setMalus(2);
+		PointModel model = (PointModel) new CustomStudentPointsFieldsConverter().convert(null, entity, null, entity.getClass());
+		Assert.assertEquals(model.getBonus(), entity.getBonus());
+		Assert.assertEquals(model.getMalus(), entity.getMalus());
+	}
+}
