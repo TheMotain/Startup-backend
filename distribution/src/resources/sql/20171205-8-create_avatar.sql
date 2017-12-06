ba8445ff141c1ed8c518fe31f1edec540d6dbd78
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2017-12-06 12:50:55 +0100

    Default value

--- distribution/src/resources/sql/20171205-8-create_avatar.sql
+++ distribution/src/resources/sql/20171205-8-create_avatar.sql
@@ -1,5 +1,5 @@
 CREATE TABLE avatar (
 	pupil integer primary key,
-	avatar_id text not null
+	avatar_id text not null default 'default',
 	constraint avatar_fk foreign key (pupil) references pupil(id) on delete cascade
 );
