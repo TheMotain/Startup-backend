f7c410cd92ebddc8e543e95bc196d31d2f0d7138
Nadia <nadia.ahassouni@gmail.com>
2017-12-20 17:23:49 +0100

    creation of table  user & entity

---
+++ data/src/main/java/fr/iagl/gamification/entity/UserEntity.java
@@ -0,0 +1,141 @@
+package fr.iagl.gamification.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entité décrivant un utilisateur en base de données
+ * @author Nadia
+ *
+ */
+
+@Entity
+@Table(name = "user")
+public class UserEntity implements Serializable{
+
+	/**
+	 * Generated Serial version UID
+	 */
+	private static final long serialVersionUID = -5944480790573583812L;
+	
+	/**
+	 * Identifiant unique de l'utilisateur
+	 */
+	@Id
+	@Column(name ="id", nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	/**
+	 * Prénom
+	 */
+	@Column( name ="firstname" , nullable = false , columnDefinition = "text")
+	private String firstName;
+	
+	/**
+	 * Nom
+	 */
+	@Column( name ="lastname" , nullable = false , columnDefinition = "text")
+	private String lastName;
+	
+	/**
+	 * email
+	 */
+	@Column( name ="email" , nullable = false , columnDefinition = "text")
+	private String email;
+	
+	/**
+	 * mot de passe
+	 */
+	@Column( name ="password" , nullable = false , columnDefinition = "text")
+	private String password;
+
+	/**
+	 * Getter de l'attribut {@link UserEntity#id}
+	 * @return id l'attribut {@link UserEntity#id}
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * Setter de l'attribut {@link UserEntity#id}
+	 * @param id l'attribut {@link UserEntity#id} à setter
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/** 
+	 * Getter de l'attirubt {@link UserEntity#firstName}
+	 * @return firstName l'attribut {@link UserEntity#firstName}
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * Setter de l'attibut {@link UserEntity#firstName}
+	 * @param firstName l'attribut {@link UserEntity#firstName} à setter
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/** 
+	 * Getter de l'attirubt {@link UserEntity#lastName}
+	 * @return lastName l'attribut {@link UserEntity#lastName}
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * Setter de l'attibut {@link UserEntity#lastName}
+	 * @param lastName l'attribut {@link UserEntity#lastName} à setter
+	 */	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/** 
+	 * Getter de l'attirubt {@link UserEntity#email}
+	 * @return email l'attribut {@link UserEntity#email}
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * Setter de l'attibut {@link UserEntity#email}
+	 * @param email l'attribut {@link UserEntity#email} à setter
+	 */	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/** 
+	 * Getter de l'attirubt {@link UserEntity#password}
+	 * @return password l'attribut {@link UserEntity#password}
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * Setter de l'attibut {@link UserEntity#password}
+	 * @param password l'attribut {@link UserEntity#password} à setter
+	 */	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+}
