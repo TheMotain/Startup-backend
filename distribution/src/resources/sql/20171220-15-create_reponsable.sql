8c8d374ffc64ca02811421682d611fd59bef7eb9
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2018-01-09 13:35:33 +0100

    correction SQL

--- distribution/src/resources/sql/20171220-15-create_reponsable.sql
+++ distribution/src/resources/sql/20171220-15-create_reponsable.sql
@@ -1,6 +1,7 @@
 create table responsable(
-	user_id integer primary key,
-	classroom_id integer primary key,
+	user_id integer,
+	classroom_id integer,
+	constraint responsable_pk primary key (user_id,classroom_id),
 	constraint user_fk foreign key (user_id) references users(id),
 	constraint classroom_fk foreign key (classroom_id) references classroom(id)
 );
