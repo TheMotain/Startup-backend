22484bf5242f153061381983ebdf78db93f0cb55
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2018-01-28 18:23:44 +0100

    exemple de format

---
+++ distribution/src/resources/sql/20180128-16-create_bought_avatar.sql
@@ -0,0 +1,15 @@
+create table avatar_price(
+	id text primary key,
+	price integer
+);
+
+create table bougth_avatar(
+	pupil integer primary key,
+	avatar_id text,
+	constraint pupil_fk foreign key (pupil) references pupil(id) on delete cascade,
+	constraint avatar_fk foreign key (avatar_id) references avatar_price(id) on delete cascade
+);
+
+insert into avatar_price select avatar_id, 0 from avatar;
+
+alter table avatar add constraint avatar_fk foreign key (avatar_id) references avatar_price(id);
