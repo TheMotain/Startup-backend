ea0552a0baa3c15ab252fd4477bb59528dbd5c21
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2018-02-06 19:49:39 +0100

    update sql

--- distribution/src/resources/sql/20180128-16-create_bought_avatar.sql
+++ distribution/src/resources/sql/20180128-16-create_bought_avatar.sql
@@ -7,11 +7,11 @@ create table bougth_avatar(
 	pupil integer, 
 	avatar_id text,
 	constraint bougth_avatar_pk primary key (pupil, avatar_id),
-	constraint pupil_fk foreign key (pupil) references pupil(id) on delete cascade, 
-	constraint avatar_fk foreign key (avatar_id) references avatar_price(id) on delete cascade
+	constraint pupil_fk foreign key (pupil) references pupil(id) on delete cascade on update cascade, 
+	constraint avatar_fk foreign key (avatar_id) references avatar_price(id) on delete cascade on update cascade
 );
 
 insert into avatar_price select distinct avatar_id, 0 from avatar;
 insert into bougth_avatar select pupil,avatar_id from avatar;
 
-alter table avatar add constraint bougth_avatar_fk foreign key (pupil,avatar_id) references bougth_avatar(pupil,avatar_id);
+alter table avatar add constraint bougth_avatar_fk foreign key (pupil,avatar_id) references bougth_avatar(pupil,avatar_id) on delete cascade on update cascade;
